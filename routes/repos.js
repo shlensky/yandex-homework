597969cc136576ed6e5d111314a882f69092d865
Dmitry Shlensky <shld@list.ru>
2019-09-15 01:44:15 +0300

    Add API for getting tree

--- routes/repos.js
+++ routes/repos.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {readdir} = require('fs').promises;
-const {getCommits, getDiff} = require('../lib/git');
+const {getCommits, getDiff, getTree} = require('../lib/git');
 const path = require('path');
 
 /* GET listing of repositories. */
@@ -20,7 +20,7 @@ router.get('/:repositoryId/commits/:commitHash', async function (req, res, next)
 
     const repos = await getRepos(req.argv.reposDir);
     if (!repos.includes(repositoryId)) {
-      res.status(400).send(`Repository ${repositoryId} does not exists`);
+      res.status(404).send(`Repository ${repositoryId} does not exists`);
       return;
     }
 
@@ -39,13 +39,33 @@ router.get('/:repositoryId/commits/:commitHash/diff', async function (req, res, 
 
     const repos = await getRepos(req.argv.reposDir);
     if (!repos.includes(repositoryId)) {
-      res.status(400).send(`Repository ${repositoryId} does not exists`);
+      res.status(404).send(`Repository ${repositoryId} does not exists`);
       return;
     }
 
     const repositoryPath = path.resolve(req.argv.reposDir, repositoryId);
     const diff = await getDiff(repositoryPath, commitHash);
     res.json(diff);
+  } catch (e) {
+    next(e);
+  }
+});
+
+
+router.get(['/:repositoryId', '/:repositoryId/tree/:commitHash/*'], async function (req, res, next) {
+  try {
+    const {repositoryId, commitHash} = req.params;
+
+    const repos = await getRepos(req.argv.reposDir);
+    if (!repos.includes(repositoryId)) {
+      res.status(404).send(`Repository ${repositoryId} does not exists`);
+      return;
+    }
+
+    const repositoryPath = path.resolve(req.argv.reposDir, repositoryId);
+    const tree = await getTree(repositoryPath, commitHash || "master", req.params[0] || ".");
+
+    res.json(tree);
   } catch (e) {
     next(e);
   }
