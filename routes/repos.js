cbd60fd0f90a4a7434752e5cf96aa6ad9358b212
Dmitry Shlensky <shld@list.ru>
2019-09-14 21:03:56 +0300

    Add API for getting list of commits

--- routes/repos.js
+++ routes/repos.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const {readdir} = require('fs').promises;
+const {getCommits} = require('../lib/git');
+const path = require('path');
 
 /* GET listing of repositories. */
 router.get('/', async function (req, res, next) {
   try {
-    res.json(await getDirs(req.argv.reposDir));
+    res.json(await getRepos(req.argv.reposDir));
   } catch (e) {
     next(e);
   }
 });
 
-async function getDirs(path) {
+/* GET listing of commits. */
+router.get('/:repositoryId/commits/:commitHash', async function (req, res, next) {
+  try {
+    const {repositoryId, commitHash} = req.params;
+
+    const repos = await getRepos(req.argv.reposDir);
+    if (!repos.includes(repositoryId)) {
+      res.status(400).send(`Repository ${repositoryId} does not exists`);
+      return;
+    }
+
+    const repositoryPath = path.resolve(req.argv.reposDir, repositoryId);
+    const commits = await getCommits(repositoryPath, commitHash);
+    res.json(commits);
+  } catch (e) {
+    next(e);
+  }
+});
+
+async function getRepos(path) {
   let files = await readdir(path, {withFileTypes: true});
   return files
     .filter(file => file.isDirectory())
