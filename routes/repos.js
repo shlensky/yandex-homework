853c95ef488e9db661bbad19c54f490c0168f312
Dmitry Shlensky <shld@list.ru>
2019-09-15 15:06:07 +0300

    Add API for cloning external repositories

--- routes/repos.js
+++ routes/repos.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const {readdir} = require('fs').promises;
-const {getCommits, getDiff, getTree, getBlob} = require('../lib/git');
+const {getRepos, cloneRepo, getCommits, getDiff, getTree, getBlob} = require('../lib/git');
 const path = require('path');
 
 /* GET listing of repositories. */
 router.get('/', async function (req, res, next) {
   try {
     res.json(await getRepos(req.argv.reposDir));
+  } catch (e) {
+    next(e);
+  }
+});
+
+/* POST clone external repository. */
+router.post('/', async function (req, res, next) {
+  try {
+    const {url} = req.query;
+    if (!url) {
+      res.status(400).send('`url` query param is required');
+      return;
+    }
+
+    await cloneRepo(req.argv.reposDir, url);
+
+    res.status(201).send();
   } catch (e) {
     next(e);
   }
@@ -99,12 +115,5 @@ router.get('/:repositoryId/blob/:commitHash/*', async function (req, res, next) 
     next(e);
   }
 });
-
-async function getRepos(path) {
-  let files = await readdir(path, {withFileTypes: true});
-  return files
-    .filter(file => file.isDirectory())
-    .map(file => file.name);
-}
 
 module.exports = router;
