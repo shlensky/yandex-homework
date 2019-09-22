82e93337d41e27d41f1c06be424f9edf1eedc8c1
Dmitry Shlensky <shld@list.ru>
2019-09-22 17:42:54 +0300

    Add styles & markup for branch select

--- markup/scripts.js
+++ markup/scripts.js
@@ -1,8 +1,25 @@
 /*
   Add scripts for demonstration purpose only.
  */
-document.querySelectorAll('.Dropdown').forEach(dropdown => {
+const dropdowns = document.querySelectorAll('.Dropdown');
+
+function closeAllDropdowns() {
+  dropdowns.forEach(dropdown => dropdown.classList.remove("Dropdown_state_opened"));
+}
+
+dropdowns.forEach(dropdown => {
   dropdown.addEventListener('click', () => {
-    dropdown.classList.toggle("Dropdown_state_opened");
+    if (dropdown.classList.contains("Dropdown_state_opened")) {
+      closeAllDropdowns();
+    } else {
+      closeAllDropdowns();
+      dropdown.classList.add("Dropdown_state_opened");
+    }
   });
+});
+
+document.body.addEventListener('click', (e) => {
+  if (!e.target.closest('.Dropdown')) {
+    closeAllDropdowns();
+  }
 });
